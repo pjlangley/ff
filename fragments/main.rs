88b49e6d7deed16422ad2cf9b9d1b0e3a78cb971
pjlangley <pjlangley@users.noreply.github.com>
2024-09-24 19:08:16 +0100

    use unwrap_or_else

--- fragments/main.rs
+++ fragments/main.rs
@@ -9,10 +9,12 @@ fn main() {
 
     println!(
         "fragment 'sql/get_item_by_ticker' output: {}",
-        sql::sql_queries::get_item_by_ticker("BTC").unwrap()
+        sql::sql_queries::get_item_by_ticker("BTC")
+            .unwrap_or_else(|| panic!("Expected BTC coin but got None"))
     );
 
-    let items_after_launch_year = sql::sql_queries::get_items_after_launch_year(2010).unwrap();
+    let items_after_launch_year = sql::sql_queries::get_items_after_launch_year(2010)
+        .unwrap_or_else(|| panic!("Expected results but got None"));
     println!(
         "fragment 'sql/get_items_after_launch_year' - found {} items",
         items_after_launch_year.len()
@@ -26,7 +28,8 @@ fn main() {
         items_after_launch_year[1]
     );
 
-    let all_items = sql::sql_queries::get_all_items().unwrap();
+    let all_items =
+        sql::sql_queries::get_all_items().unwrap_or_else(|| panic!("Expected coins but got None"));
     println!(
         "fragment 'sql/get_all_items' - found {} items",
         all_items.len()
