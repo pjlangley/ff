53a7312540212ecfc6756121aca84590b2112392
pjlangley <pjlangley@users.noreply.github.com>
2025-05-09 07:16:40 +0100

    rm commitement opt - already set in rpc client

--- fragments/solana_transaction/solana_transaction_utils.py
+++ fragments/solana_transaction/solana_transaction_utils.py
@@ -1,11 +1,10 @@
 from solders.signature import Signature
-from solana.rpc.api import Commitment
 from fragments.solana_rpc import init_rpc_client
 
 
 def confirm_recent_signature(signature: Signature) -> bool:
     client = init_rpc_client()
-    response = client.confirm_transaction(signature, Commitment("confirmed"))
+    response = client.confirm_transaction(signature)
     status = response.value[0]
 
     return bool(status is not None and status.confirmations is not None)
