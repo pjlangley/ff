0690db8f1159bdf23d195290aab2789fa4b2edbc
pjlangley <pjlangley@users.noreply.github.com>
2025-10-16 20:21:36 +0100

    kebab -> snake case file names

--- fragments/api/fastify/app.ts
+++ fragments/api/fastify/app.ts
@@ -2,7 +2,7 @@ import fastify from "fastify";
 import { routes as postgres } from "./datastore/postgres";
 import { routes as redis } from "./datastore/redis";
 import { routes as sqlite } from "./datastore/sqlite";
-import { routes as solanaCounter } from "./blockchain/solana-counter";
+import { routes as solanaCounter } from "./blockchain/solana_counter";
 import { routes as solanaUsername } from "./blockchain/solana_username";
 
 export const build = () => {
